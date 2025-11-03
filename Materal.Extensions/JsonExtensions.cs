09c21f6da56241fe31214e07d273d82b8a6af870
陈明旭 <cloomcmx1554@hotmail.com>
2025-11-03 14:25:12 +0800

    refactor: 移除多个工具类并优化JSON扩展方法

--- Materal.Extensions/JsonExtensions.cs
+++ Materal.Extensions/JsonExtensions.cs
@@ -74,19 +74,6 @@ public static class JsonExtensions
         }
     }
     /// <summary>
-    /// Json字符串转换对象
-    /// </summary>
-    /// <typeparam name="T">接口类型</typeparam>
-    /// <param name="jsonStr"></param>
-    /// <param name="typeName"></param>
-    /// <returns></returns>
-    /// <exception cref="ExtensionException"></exception>
-    public static T JsonToInterface<T>(this string jsonStr, string typeName)
-    {
-        Type triggerDataType = typeName.GetTypeByTypeName<T>() ?? throw new ExtensionException("转换失败");
-        return (T)jsonStr.JsonToObject(triggerDataType);
-    }
-    /// <summary>
     /// 对象转换为Json
     /// </summary>
     /// <param name="obj">要转换的对象</param>
