e92d52d530058a9c092bd18327535531c5457e7d
陈明旭 <cloomcmx1554@hotmail.com>
2025-11-21 10:38:01 +0800

    feat: 增强 JSON 序列化与反序列化功能

--- Materal.Extensions/JsonExtensions.cs
+++ Materal.Extensions/JsonExtensions.cs
@@ -1,5 +1,6 @@
-﻿using Newtonsoft.Json;
-using System.Xml;
+﻿using Materal.Extensions.JsonConverters;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Materal.Extensions;
 
@@ -11,26 +12,31 @@ public static class JsonExtensions
     /// <summary>
     /// Json序列化配置
     /// </summary>
-    public static JsonSerializerSettings JsonSerializerSettings { get; } = new()
-    {
-    };
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = GetDefaultJsonSerializerOptions();
+
     /// <summary>
-    /// Json转换为XML文档对象
+    /// 获得默认的Json序列化配置
     /// </summary>
-    /// <param name="jsonStr">json字符串</param>
-    /// <returns>XML文档对象</returns>
-    public static XmlDocument? JsonToXml(this string jsonStr) => JsonConvert.DeserializeXmlNode(jsonStr);
+    /// <returns></returns>
+    private static JsonSerializerOptions GetDefaultJsonSerializerOptions() => new()
+    {
+        Converters = { new JsonStringEnumConverter() },//自定义转换器
+        PropertyNameCaseInsensitive = true,// 忽略大小写
+        WriteIndented = true,// 格式化输出
+    };
+
     /// <summary>
     /// Json字符串转换对象
     /// </summary>
     /// <param name="jsonStr">Json字符串</param>
     /// <param name="type"></param>
+    /// <param name="options"></param>
     /// <returns>转换后的对象</returns>
-    public static object JsonToObject(this string jsonStr, Type type)
+    public static object JsonToObject(this string jsonStr, Type type, JsonSerializerOptions? options = null)
     {
         try
         {
-            object model = JsonConvert.DeserializeObject(jsonStr, type, JsonSerializerSettings) ?? throw new ExtensionException("转换失败");
+            object? model = JsonSerializer.Deserialize(jsonStr, type, options ?? _jsonSerializerOptions) ?? throw new ExtensionException("转换失败");
             return model;
         }
         catch (Exception ex)
@@ -38,34 +44,19 @@ public static class JsonExtensions
             throw new ExtensionException("Json字符串有误", ex);
         }
     }
-    /// <summary>
-    /// Json字符串转换对象
-    /// </summary>
-    /// <param name="jsonStr">Json字符串</param>
-    /// <returns>转换后的对象</returns>
-    public static object JsonToObject(this string jsonStr)
-    {
-        try
-        {
-            object model = JsonConvert.DeserializeObject(jsonStr, JsonSerializerSettings) ?? throw new ExtensionException("转换失败");
-            return model;
-        }
-        catch (Exception ex)
-        {
-            throw new ExtensionException("Json字符串有误", ex);
-        }
-    }
+
     /// <summary>
     /// Json字符串转换对象
     /// </summary>
     /// <typeparam name="T">目标对象类型</typeparam>
     /// <param name="jsonStr">Json字符串</param>
+    /// <param name="options"></param>
     /// <returns>转换后的对象</returns>
-    public static T JsonToObject<T>(this string jsonStr)
+    public static T JsonToObject<T>(this string jsonStr, JsonSerializerOptions? options = null)
     {
         try
         {
-            T model = JsonConvert.DeserializeObject<T>(jsonStr, JsonSerializerSettings) ?? throw new ExtensionException("转换失败");
+            T? model = JsonSerializer.Deserialize<T>(jsonStr, options ?? _jsonSerializerOptions) ?? throw new ExtensionException("转换失败");
             return model;
         }
         catch (Exception ex)
@@ -73,10 +64,87 @@ public static class JsonExtensions
             throw new ExtensionException("Json字符串有误", ex);
         }
     }
+
+    /// <summary>
+    /// Json字符串转换对象
+    /// </summary>
+    /// <param name="jsonStr">Json字符串</param>
+    /// <param name="options"></param>
+    /// <returns>转换后的对象</returns>
+    public static object JsonToObject(this string jsonStr, JsonSerializerOptions? options = null) => JsonToObject<object>(jsonStr, options);
+
     /// <summary>
     /// 对象转换为Json
     /// </summary>
     /// <param name="obj">要转换的对象</param>
+    /// <param name="options"></param>
     /// <returns>转换后的Json字符串</returns>
-    public static string ToJson(this object obj) => JsonConvert.SerializeObject(obj, JsonSerializerSettings);
+    public static string ToJson(this object obj, JsonSerializerOptions? options = null) => JsonSerializer.Serialize(obj, options ?? _jsonSerializerOptions);
+
+    /// <summary>
+    /// 对象转换为Json，保留类型信息以便还原
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    public static string ToJsonWithInferredTypes(this object obj, JsonSerializerOptions? options = null)
+    {
+        JsonSerializerOptions serializerOptions = options ?? GetDefaultJsonSerializerOptions();
+        serializerOptions.Converters.Add(new ObjectToInferredTypesConverter());
+        return JsonSerializer.Serialize(obj, serializerOptions);
+    }
+
+    /// <summary>
+    /// Json字符串转换对象，自动还原类型信息
+    /// </summary>
+    /// <param name="jsonStr"></param>
+    /// <param name="type"></param>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    /// <exception cref="ExtensionException"></exception>
+    public static object JsonToObjectWithInferredTypes(this string jsonStr, Type type, JsonSerializerOptions? options = null)
+    {
+        try
+        {
+            JsonSerializerOptions serializerOptions = options ?? GetDefaultJsonSerializerOptions();
+            serializerOptions.Converters.Add(new ObjectToInferredTypesConverter());
+            object model = JsonSerializer.Deserialize(jsonStr, type, serializerOptions) ?? throw new ExtensionException("转换失败");
+            return model;
+        }
+        catch (Exception ex)
+        {
+            throw new ExtensionException("Json字符串有误", ex);
+        }
+    }
+
+    /// <summary>
+    /// Json字符串转换对象，自动还原类型信息
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="jsonStr"></param>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    /// <exception cref="ExtensionException"></exception>
+    public static T JsonToObjectWithInferredTypes<T>(this string jsonStr, JsonSerializerOptions? options = null)
+    {
+        try
+        {
+            JsonSerializerOptions serializerOptions = options ?? GetDefaultJsonSerializerOptions();
+            serializerOptions.Converters.Add(new ObjectToInferredTypesConverter());
+            T? model = JsonSerializer.Deserialize<T>(jsonStr, serializerOptions) ?? throw new ExtensionException("转换失败");
+            return model;
+        }
+        catch (Exception ex)
+        {
+            throw new ExtensionException("Json字符串有误", ex);
+        }
+    }
+
+    /// <summary>
+    /// Json字符串转换对象，自动还原类型信息
+    /// </summary>
+    /// <param name="jsonStr"></param>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    public static object JsonToObjectWithInferredTypes(this string jsonStr, JsonSerializerOptions? options = null) => JsonToObjectWithInferredTypes<object>(jsonStr, options);
 }
--- Materal.Extensions/XmlExtensions.cs
+++ Materal.Extensions/XmlExtensions.cs
@@ -1,77 +1,63 @@
-﻿using Newtonsoft.Json;
-using System.Xml;
+﻿using System.Xml;
 using System.Xml.Serialization;
 
-namespace Materal.Extensions
+namespace Materal.Extensions;
+
+/// <summary>
+/// XML扩展
+/// </summary>
+public static class XmlExtensions
 {
     /// <summary>
-    /// XML扩展
+    /// 对象转换为Xml
     /// </summary>
-    public static class XmlExtensions
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public static string ToXml(this object obj)
     {
-        /// <summary>
-        /// XML文档对象转换为Json字符串
-        /// </summary>
-        /// <param name="xmlDoc">XML文档对象</param>
-        /// <returns>Json字符串</returns>
-        public static string ToJson(this XmlDocument xmlDoc) => JsonConvert.SerializeXmlNode(xmlDoc);
-        /// <summary>
-        /// XML节点对象转换为Json字符串
-        /// </summary>
-        /// <param name="xmlNode">XML节点对象</param>
-        /// <returns>Json字符串</returns>
-        public static string ToJson(this XmlNode xmlNode) => JsonConvert.SerializeXmlNode(xmlNode);
-        /// <summary>
-        /// 对象转换为Xml
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
-        public static string ToXml(this object obj)
+        if (obj is IDictionary<string, object> dictionary) return dictionary.ToXml();
+        XmlSerializer serializer;
+        using StringWriter stringWriter = new();
+        serializer = new(obj.GetType());
+        serializer.Serialize(stringWriter, obj);
+        return stringWriter.ToString();
+    }
+    /// <summary>
+    /// 对象转换为Xml
+    /// </summary>
+    /// <param name="dictionary"></param>
+    /// <returns></returns>
+    public static string ToXml(this IDictionary<string, object> dictionary)
+    {
+        XmlDocument xmlDocument = new();
+        XmlNode xmlNode = xmlDocument.CreateElement("Root");
+        foreach (KeyValuePair<string, object> item in dictionary)
         {
-            if (obj is IDictionary<string, object> dictionary) return dictionary.ToXml();
-            XmlSerializer serializer;
-            using StringWriter stringWriter = new();
-            serializer = new(obj.GetType());
-            serializer.Serialize(stringWriter, obj);
-            return stringWriter.ToString();
+            xmlNode.AppendChild(xmlDocument, item);
         }
-        /// <summary>
-        /// 对象转换为Xml
-        /// </summary>
-        /// <param name="dictionary"></param>
-        /// <returns></returns>
-        public static string ToXml(this IDictionary<string, object> dictionary)
+        xmlDocument.AppendChild(xmlNode);
+        return xmlDocument.OuterXml;
+    }
+    /// <summary>
+    /// 添加子级
+    /// </summary>
+    /// <param name="node"></param>
+    /// <param name="xmlDocument"></param>
+    /// <param name="item"></param>
+    private static void AppendChild(this XmlNode node, XmlDocument xmlDocument, KeyValuePair<string, object> item)
+    {
+        XmlNode newNode = xmlDocument.CreateElement(item.Key);
+        if (item.Value is IDictionary<string, object> dictionary)
         {
-            XmlDocument xmlDocument = new();
-            XmlNode xmlNode = xmlDocument.CreateElement("Root");
-            foreach (KeyValuePair<string, object> item in dictionary)
+            foreach (KeyValuePair<string, object> child in dictionary)
             {
-                xmlNode.AppendChild(xmlDocument, item);
+                newNode.AppendChild(xmlDocument, child);
             }
-            xmlDocument.AppendChild(xmlNode);
-            return xmlDocument.OuterXml;
         }
-        /// <summary>
-        /// 添加子级
-        /// </summary>
-        /// <param name="node"></param>
-        /// <param name="xmlDocument"></param>
-        /// <param name="item"></param>
-        private static void AppendChild(this XmlNode node, XmlDocument xmlDocument, KeyValuePair<string, object> item)
+        else
         {
-            XmlNode newNode = xmlDocument.CreateElement(item.Key);
-            if (item.Value is IDictionary<string, object> dictionary)
-            {
-                foreach (KeyValuePair<string, object> child in dictionary)
-                {
-                    newNode.AppendChild(xmlDocument, child);
-                }
-            }
-            else
-            {
-                newNode.InnerText = item.Value.ToString() ?? string.Empty;
-            }
-            node.AppendChild(newNode);
+            newNode.InnerText = item.Value.ToString() ?? string.Empty;
         }
+        node.AppendChild(newNode);
     }
 }
